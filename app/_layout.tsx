2835bc41fbb3d78379915ac2ffbd0728c0fe8d9c
Ana Morón-Cervantes <anamoron-cervantes@Anas-MacBook-Air-2.local>
2026-02-07 21:00:10 -0600

    Hide root stack header to remove (tabs) on web

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Interview Prep" }} />
-      <Stack.Screen name="levels/1/index" options={{ title: "Level 1: Resume" }} />
-      <Stack.Screen name="levels/1/module/[moduleId]" options={{ title: "Level 1 Module" }} />
-    </Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false, // ✅ THIS is the key line
+      }}
+    />
   );
 }
